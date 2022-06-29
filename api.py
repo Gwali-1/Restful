757a2209582d176b76ba820daf58a5272ce68962
Gwali <gwalisam37@gmail.com>
2022-06-29 13:25:55 +0000

    added implementation for functions that genrate auth tokens and a function that adds a new user with information provided:

--- api.py
+++ api.py
@@ -1,10 +1,15 @@
 from asyncio import FastChildWatcher
+from lib2to3.pgen2 import token
 import time
 import redis
 import json
+
+from redis import RedisError
+from redis import AuthenticationError
 from c import creatapp
 from redis import StrictRedis
 from flask_restful import Resource,Api,abort
+import secrets
 
 config = {"redis_host":"localhost","redis_port":6379}
 redis_conn = StrictRedis(config["redis_host"],config["redis_port"],decode_responses=True)
@@ -12,13 +17,11 @@ app = creatapp()
 
 
 def validate_user_password(username,password):
-    users = redis_conn.hgetall("userConfig")
+    users = redis_conn.hgetall("UsersD")
     if username in users.keys():
         if users[username] == password:
             return True
     return False
-
-
 
 
 
@@ -30,3 +33,26 @@ def validate_user_token(user_token,id):
             return True
     print("authentication failled")
     return False
+
+
+
+def  generate_auth_token():
+    auth_token = secrets.token_urlsafe()
+    auth_id = secrets.token_hex(2)
+    try:
+        redis_conn.set(auth_id,auth_token)
+        redis_conn.expire(auth_id,3600)
+        return True
+    except (RedisError,AuthenticationError ,ConnectionError) as error:
+        print(error)
+        return False
+
+
+def add_user(username,password):
+    user_info = {username:password}
+    try:
+        redis_conn.hset("UsersD",mapping=user_info)
+        return True
+    except (RedisError,AuthenticationError ,ConnectionError) as error:
+        print(error)
+        return False
